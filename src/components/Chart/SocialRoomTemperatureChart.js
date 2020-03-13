b0a3732a72bd637e8e1cc31acd428c6abf092a2b
Ubi <ub_g@outlook.com>
2020-03-13 13:21:31 +0000

    feedback for emoji on speakre view

--- src/components/Chart/SocialRoomTemperatureChart.js
+++ src/components/Chart/SocialRoomTemperatureChart.js
@@ -1,43 +1,39 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-import CanvasJSReact from "../../canvasjs.react";
+import CanvasJSReact from '../../canvasjs.react';
 import * as selectors from '.././../selectors';
-import {signInByEmailAndPassword, getAllVotesForSession, fetchSession, sumbitVote, setSessionTimeFrame, startSession, endSession} from '../../actions';
+import { sumbitVote, setSessionTimeFrame, startSession, endSession } from '../../actions';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-class SocialRoomTemperatureChart extends React.Component{
-    componentDidMount(){
-        console.warn({sessionId: this.props.sessionId})
-        this.props.getAllVotesForSession(this.props.sessionId);
-    }
-
-    render(){
+class SocialRoomTemperatureChart extends React.Component {
+    render() {
         const options = {
-            title: {
-                text: "Social Room Temperature"
-            },
-            data: [{
-                type: "line",
-                dataPoints: this.props.chartData
-            }]
-        }
-        return(
+            data: [
+                {
+                    type: 'line',
+                    dataPoints: this.props.chartData || []
+                }
+            ]
+        };
+        return (
             <div>
-                <CanvasJSChart options = {options} onRef={ref => this.chart = ref} />
+                <CanvasJSChart options={options} onRef={ref => (this.chart = ref)} />
             </div>
-        )
+        );
     }
 }
 
 const mapStateToProps = state => {
     return {
-        chartData: selectors.getChartDataForAllVotes(state),
         sessionId: selectors.getCurrentSessionId(state)
     };
 };
 
-export default connect(mapStateToProps, {signInByEmailAndPassword, getAllVotesForSession, fetchSession, sumbitVote, setSessionTimeFrame, startSession, endSession})(
-    SocialRoomTemperatureChart
-);
+export default connect(mapStateToProps, {
+    sumbitVote,
+    setSessionTimeFrame,
+    startSession,
+    endSession
+})(SocialRoomTemperatureChart);
--- src/components/RoomView/SpeakerRoomView.js
+++ src/components/RoomView/SpeakerRoomView.js
@@ -1,61 +1,81 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { Typography, Chip, Button } from "@material-ui/core";
-import { theme } from "../../config/theme";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Chip, Button } from '@material-ui/core';
+import { theme } from '../../config/theme';
 
 import SocialRoomTemperatureChart from '../Chart/SocialRoomTemperatureChart';
 
 const styles = {
-  root: { margin: "24px" },
-  commentList: { display: "inline-grid" },
-  comment: { margin: "6px" },
-  button: {
-    width: "fit-content",
-    alignSelf: "flex-end",
-    background: theme.colours.accent
-  }
+    root: { margin: '24px' },
+    commentList: { display: 'inline-grid' },
+    comment: { margin: '6px' },
+    button: {
+        width: 'fit-content',
+        alignSelf: 'flex-end',
+        background: theme.colours.accent
+    }
 };
 
-class SpeakerRoomView extends React.Component{
-  componentDidMount(){
-    let timeFrame = 1;
-    setInterval(() => {
-      this.props.setSessionTimeFrame(this.props.sessionDetails.id, timeFrame);
-      timeFrame++
-    }, 10000)
-  }
+class SpeakerRoomView extends React.Component {
+    componentDidMount() {
+        let timeFrame = 1;
+        setInterval(() => {
+            this.props.setSessionTimeFrame(this.props.sessionDetails.id, timeFrame);
+            timeFrame++;
+        }, 10000);
+    }
 
-  render(){
-    const { classes, comments, handleCloseRoom, sessionDetails } = this.props;
+    getEmoji = value => {
+        console.warn('value', value);
+        if (value <= 20) {
+            return '😫';
+        } else if (value <= 40 && value > 20) {
+            return '😕 ';
+        } else if (value <= 60 && value > 40) {
+            return '😐';
+        } else if (value <= 80 && value > 60) {
+            return '🙂';
+        } else if (value > 80) return '🤩';
+    };
 
-    return(
-      <div className={classes.root}>
-      <Typography variant="h3">{sessionDetails.id}</Typography>
-      <Typography variant="h1">
-        <span role="img" aria-label="emoji">
-          😍
-        </span>
-      </Typography>
-      <div className={classes.commentList}>
-        {comments.map(comment => (
-          <Chip label={comment} className={classes.comment} key={comment} />
-        ))}
-      </div>
-      <div>
-        <SocialRoomTemperatureChart />
-      </div>
-      <div>
-        <Button
-          className={classes.button}
-          variant="outlined"
-          onClick={() => handleCloseRoom()}
-        >
-          Close Room
-        </Button>
-      </div>
-    </div>
-    )
-  }
+    render() {
+        const {
+            classes,
+            comments,
+            handleCloseRoom,
+            sessionDetails,
+            chartData,
+            timeInterval
+        } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <Typography variant="h3">{sessionDetails.id}</Typography>
+                <Typography variant="h1">
+                    <span role="img" aria-label="emoji">
+                        {this.getEmoji(chartData[timeInterval] ? chartData[timeInterval].y : 50)}
+                    </span>
+                </Typography>
+                <div className={classes.commentList}>
+                    {comments.map(comment => (
+                        <Chip label={comment} className={classes.comment} key={comment} />
+                    ))}
+                </div>
+                <div>
+                    <SocialRoomTemperatureChart chartData={chartData} />
+                </div>
+                <div>
+                    <Button
+                        className={classes.button}
+                        variant="outlined"
+                        onClick={() => handleCloseRoom()}
+                    >
+                        Close Room
+                    </Button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(SpeakerRoomView);
